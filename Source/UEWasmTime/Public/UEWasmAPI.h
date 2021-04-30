d50f4a6654d861630a8645f8160a77de8811be91
MarkJGx <mark.jg@chemicalheads.com>
2021-04-30 09:43:35 +0300

    wrapped wasm_valtype_t

--- Source/UEWasmTime/Public/UEWasmAPI.h
+++ Source/UEWasmTime/Public/UEWasmAPI.h
@@ -6,7 +6,7 @@ THIRD_PARTY_INCLUDES_START
 THIRD_PARTY_INCLUDES_END
 
 
-#define DECLARE_CUSTOM_WASMTYPE(Name, WasmType, DeleterFunction) \
+#define DECLARE_CUSTOM_WASMTYPE_CUSTOM(Name, SmartPtrType, WasmType, DeleterFunction) \
 struct T##Name##CustomDeleter : TDefaultDelete<WasmType> \
 {\
 	bool bDontDelete;\
@@ -26,7 +26,19 @@ struct T##Name##CustomDeleter : TDefaultDelete<WasmType> \
 		}\
 	}\
 };\
-typedef TUniquePtr<WasmType, T##Name##CustomDeleter> T##Name
+typedef SmartPtrType<WasmType, T##Name##CustomDeleter> T##Name
+
+/**
+ * Declares WASM type TUniquePtr.
+ */
+#define DECLARE_CUSTOM_WASMTYPE(Name, WasmType, DeleterFunction) \
+	DECLARE_CUSTOM_WASMTYPE_CUSTOM(Name, TUniquePtr, WasmType, DeleterFunction)
+
+/**
+* Declares WASM type TUniquePtr.
+*/
+#define DECLARE_CUSTOM_WASMTYPE_SHARED_PTR(Name, WasmType, DeleterFunction) \
+	DECLARE_CUSTOM_WASMTYPE_CUSTOM(Name, TSharedPtr, WasmType, DeleterFunction)
 
 template <typename T>
 struct TWasmRef
@@ -102,62 +114,6 @@ FORCEINLINE bool HandleError(const FString& Caller, wasmtime_error_t* ErrorPoint
  */
 namespace UEWas
 {
-	template <typename T>
-	struct TWasmValue
-	{
-	};
-
-	template <>
-	struct TWasmValue<int32_t>
-	{
-		static wasm_val_t NewValue(int32_t InValue)
-		{
-			wasm_val_t WasmValue;
-			WasmValue.kind = WASM_I32;
-			WasmValue.of.i32 = InValue;
-			return WasmValue;
-		}
-
-		static wasm_valtype_t* GetType()
-		{
-			return wasm_valtype_new_i32();
-		}
-	};
-
-	template <>
-	struct TWasmValue<int64_t>
-	{
-		static wasm_val_t NewValue(int64_t InValue)
-		{
-			wasm_val_t WasmValue;
-			WasmValue.kind = WASM_I64;
-			WasmValue.of.i64 = InValue;
-			return WasmValue;
-		}
-		
-		static wasm_valtype_t* GetType()
-		{
-			return wasm_valtype_new_i64();
-		}
-	};
-
-	template <>
-	struct TWasmValue<wasm_ref_t*>
-	{
-		static wasm_val_t NewValue(wasm_ref_t* InValue)
-		{
-			wasm_val_t WasmValue;
-			WasmValue.kind = WASM_ANYREF;
-			WasmValue.of.ref = InValue;
-			return WasmValue;
-		}
-		
-		static wasm_valtype_t* GetType()
-		{
-			return wasm_valtype_new_anyref();
-		}
-	};
-
 	DECLARE_CUSTOM_WASMTYPE(WasiConfig, wasi_config_t, wasi_config_delete);
 	DECLARE_CUSTOM_WASMTYPE(WasiInstance, wasi_instance_t, wasi_instance_delete);
 
@@ -168,20 +124,35 @@ namespace UEWas
 	DECLARE_CUSTOM_WASMTYPE(WasmFuncType, wasm_functype_t, wasm_functype_delete);
 	DECLARE_CUSTOM_WASMTYPE(WasmFunc, wasm_func_t, wasm_func_delete);
 	DECLARE_CUSTOM_WASMTYPE(WasmLinker, wasmtime_linker_t, wasmtime_linker_delete);
-
-
+	
+	// VEC types have an overhead of an additional pointer.
 	DECLARE_CUSTOM_WASMTYPE_VEC(WasmByteVec, wasm_byte_vec_t, wasm_byte_t, wasm_byte_vec_new, wasm_byte_vec_delete);
 	DECLARE_CUSTOM_WASMTYPE_VEC_CONST(WasmExternVec, wasm_extern_vec_t, wasm_extern_t, wasm_extern_vec_new, wasm_extern_vec_delete);
 	DECLARE_CUSTOM_WASMTYPE_VEC_CONST(WasmValTypeVec, wasm_valtype_vec_t, wasm_valtype_t, wasm_valtype_vec_new, wasm_valtype_vec_delete);
-
 	DECLARE_CUSTOM_WASMTYPE_VEC(WasmValVec, wasm_val_vec_t, wasm_val_t, wasm_val_vec_new, wasm_val_vec_delete);
 
+
+	 // TSharedPtr checks for type completeness so we use std::shared_ptr instead.
+	typedef std::shared_ptr<wasm_valtype_t> TWasmValType;
 	
 	typedef wasm_extern_t* TWasmExtern;
 	typedef TWasmByteVec TWasmName;
 
-	FORCEINLINE TWasmName MakeWasmName(const FString& InString)
+	FORCEINLINE TWasmValType MakeWasmValType(wasm_valtype_t* ValType)
 	{
+		check(ValType);
+		auto Test = TWasmValType(ValType, [](wasm_valtype_t* ValType)
+		{
+			if(ValType)
+			{
+				wasm_valtype_delete(ValType);
+			}
+		});
+		return Test;
+	}
+
+	FORCEINLINE TWasmName MakeWasmName(const FString& InString)
+	{		
 		auto NamePtr = new TWasmRef<wasm_name_t>();
 		if(!InString.IsEmpty())
 		{
@@ -241,11 +212,30 @@ namespace UEWas
 	FORCEINLINE VecArrayType MakeWasmVec(typename VecArrayType::StaticElementType* Data, const uint32& Num, bool bDontDelete = false)
 	{
 		const auto Vec = new TWasmRef<typename VecArrayType::StaticWasmType>();
-		if(Data != nullptr && Num != 0)
+		if (Data != nullptr && Num != 0)
 		{
 			VecArrayType::Allocate(Vec->Value, Data, Num);
-		} 
+		}
 		return VecArrayType(Vec, typename VecArrayType::StaticWasmDeleter(bDontDelete));
+	}
+
+	FORCEINLINE TWasmValTypeVec MakeWasmValTypeVecConst(TWasmValType* Data, const uint32& Num,
+	                                                    bool bDontDelete = false)
+	{
+		TArray<wasm_valtype_t*> InnerValTypes;
+		for (uint32 Index = 0; Index < Num; Index++)
+		{
+			check(Data[Index].get());
+			InnerValTypes.Emplace(Data[Index].get());
+		}
+		return MakeWasmVecConst<TWasmValTypeVec>(InnerValTypes.GetData(), InnerValTypes.Num(), bDontDelete);
+	}
+
+	FORCEINLINE TWasmValTypeVec MakeWasmValTypeVecConst(TArray<TWasmValType>& Array,
+													bool bDontDelete = false)
+	{
+		
+		return MakeWasmValTypeVecConst(Array.GetData(), Array.Num(), bDontDelete);
 	}
 
 	FORCEINLINE TWasmFuncType MakeWasmFuncType(TWasmValTypeVec&& Params, TWasmValTypeVec&& Results)
@@ -253,6 +243,35 @@ namespace UEWas
 		return TWasmFuncType(wasm_functype_new(&Params.Get()->Value, &Results.Get()->Value));
 	}
 
+	FORCEINLINE TWasmValType MakeWasmValTypeInt32()
+	{
+		return MakeWasmValType(wasm_valtype_new_i32());
+	}
+	
+	FORCEINLINE TWasmValType MakeWasmValTypeInt64()
+	{
+		return MakeWasmValType(wasm_valtype_new_i64());
+	}
+	
+	FORCEINLINE TWasmValType MakeWasmValTypeFloat32()
+	{
+		return MakeWasmValType(wasm_valtype_new_f32());
+	}
+	
+	FORCEINLINE TWasmValType MakeWasmValTypeFloat64()
+	{
+		return MakeWasmValType(wasm_valtype_new_f64());
+	}
+	
+	FORCEINLINE TWasmValType MakeWasmValTypeAnyRef()
+	{
+		return MakeWasmValType(wasm_valtype_new_anyref());
+	}
+	
+	FORCEINLINE TWasmValType MakeWasmValTypeFuncRef()
+	{
+		return MakeWasmValType(wasm_valtype_new_funcref());
+	}
 
 	FORCEINLINE TWasmLinker MakeWasmLinker(const TWasiInstance& WasiInstance, const TWasmStore& Store)
 	{
@@ -266,6 +285,9 @@ namespace UEWas
 
 	FORCEINLINE TWasiInstance MakeWasiInstance(const TWasmStore& Store, TWasiConfig&& Config)
 	{
+		check(Store.Get());
+		check(Config.Get());
+		
 		wasm_trap_t* Trap;
 		wasi_instance_t* WasiInstance = wasi_instance_new(Store.Get(), "wasi_snapshot_preview1", Config.Release(), &Trap);
 		if (!WasiInstance)
@@ -286,6 +308,7 @@ namespace UEWas
 		wasmtime_error_t* Error = wasmtime_linker_instantiate(Linker.Get(), Module.Get(), &RawInstance, &Trap);
 		TWasmInstance WasmInstance = TWasmInstance(RawInstance);
 		HandleError(TEXT("New Instance"), Error, nullptr);
+		
 		return WasmInstance;
 	}
 
@@ -311,4 +334,94 @@ namespace UEWas
 		check(Binary.Get());
 		return TWasmModule(wasm_module_new(Store.Get(), &Binary.Get()->Value));
 	}
+
+	template <typename T>
+	struct TWasmValue
+	{
+	};
+
+	template <>
+	struct TWasmValue<int32_t>
+	{
+		static wasm_val_t NewValue(int32_t InValue)
+		{
+			wasm_val_t WasmValue;
+			WasmValue.kind = WASM_I32;
+			WasmValue.of.i32 = InValue;
+			return WasmValue;
+		}
+
+		static TWasmValType GetType()
+		{
+			return MakeWasmValTypeInt32();
+		}
+	};
+
+	template <>
+	struct TWasmValue<int64_t>
+	{
+		static wasm_val_t NewValue(int64_t InValue)
+		{
+			wasm_val_t WasmValue;
+			WasmValue.kind = WASM_I64;
+			WasmValue.of.i64 = InValue;
+			return WasmValue;
+		}
+
+		static TWasmValType GetType()
+		{
+			return MakeWasmValTypeInt64();
+		}
+	};
+
+	template <>
+	struct TWasmValue<float32_t>
+	{
+		static wasm_val_t NewValue(float32_t InValue)
+		{
+			wasm_val_t WasmValue;
+			WasmValue.kind = WASM_F32;
+			WasmValue.of.f32 = InValue;
+			return WasmValue;
+		}
+
+		static TWasmValType GetType()
+		{
+			return MakeWasmValTypeFloat32();
+		}
+	};
+
+	template <>
+	struct TWasmValue<float64_t>
+	{
+		static wasm_val_t NewValue(float64_t InValue)
+		{
+			wasm_val_t WasmValue;
+			WasmValue.kind = WASM_F64;
+			WasmValue.of.f64 = InValue;
+			return WasmValue;
+		}
+
+		static TWasmValType GetType()
+		{
+			return MakeWasmValTypeFloat64();
+		}
+	};
+
+	template <>
+	struct TWasmValue<wasm_ref_t*>
+	{
+		static wasm_val_t NewValue(wasm_ref_t* InValue)
+		{
+			wasm_val_t WasmValue;
+			WasmValue.kind = WASM_ANYREF;
+			WasmValue.of.ref = InValue;
+			return WasmValue;
+		}
+
+		static TWasmValType GetType()
+		{
+			return MakeWasmValTypeAnyRef();
+		}
+	};
 }
